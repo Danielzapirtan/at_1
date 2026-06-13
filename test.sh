dfe3babcff4d4e2ce17b389d09b816768522e11d
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-06-13 10:17:53 +0300

    Sat Jun 13 10:17:53 EEST 2026

--- test.sh
+++ test.sh
@@ -1,34 +1,26 @@
+#! /usr/bin/env bash
+
 PROJECT="at_1"
 DIR=$HOME/"$PROJECT"
 MACOS=$(uname | grep -q Darwin)
 echo $MACOS
 ${PRODUCTION:=true}
 
-if $MACOS; then
-	if ! $PRODUCTION; then
-		export HOMEBREW_NO_REQUIRE_TAP_TRUST=1
-		command -v brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-		brew update
-		brew upgrade
-		brew install python@3.11
-		brew install ffmpeg-full
-	fi
-else
-	apt update
-	apt install ffmpeg
-	cd $HOME
-	rm -rf $DIR
-	echo $PROJECT
-	git clone https://github.com/Danielzapirtan/"$PROJECT"
-	cd $DIR
+if ! $MACOS; then
+	echo "Platform not supoorted"
+	exit 2
 fi
-pithon -m pip install --upgrade pip
+if ! $PRODUCTION; then
+	export HOMEBREW_NO_REQUIRE_TAP_TRUST=1
+	command -v brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	brew update
+	brew upgrade
+	brew install python@3.12
+	brew install ffmpeg-full
+fi
+python -m pip install --upgrade pip
 pip install -r requirements.txt
-if $MACOS; then
-	pip install --force-reinstall "faster-whisper @ https://github.com/SYSTRAN/faster-whisper/archive/refs/heads/master.tar.gz"
-else
-	pip install faster-whisper
-fi
+pip install --force-reinstall "faster-whisper @ https://github.com/SYSTRAN/faster-whisper/archive/refs/heads/master.tar.gz"
 python3 app.py $HOME/default.m4a
 cat transcription.txt
 
