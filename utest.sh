08d2db700d44e37a1c5ca76b4590420224696d1b
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-06-13 10:19:36 +0300

    Sat Jun 13 10:19:36 EEST 2026

--- utest.sh
+++ utest.sh
@@ -2,14 +2,7 @@
 
 PROJECT="at_1"
 DIR=$HOME/"$PROJECT"
-MACOS=$(uname | grep -q Darwin)
-echo $MACOS
 ${PRODUCTION:=true}
-
-if $MACOS; then
-	echo "Platform not supported"
-	exit 2
-fi
 apt update
 apt install ffmpeg
 cd $HOME
@@ -19,11 +12,7 @@ git clone https://github.com/Danielzapirtan/"$PROJECT"
 cd $DIR
 python -m pip install --upgrade pip
 pip install -r requirements.txt
-if $MACOS; then
-	pip install --force-reinstall "faster-whisper @ https://github.com/SYSTRAN/faster-whisper/archive/refs/heads/master.tar.gz"
-else
-	pip install faster-whisper
-fi
+pip install faster-whisper
 python3 app.py $HOME/default.m4a
 cat transcription.txt
 
