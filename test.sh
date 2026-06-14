ef5e0f42c5f3cf7c5e5a56dee3177118dbe0ace2
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-06-14 14:48:17 +0300

    Sun Jun 14 14:48:17 EEST 2026

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 PROJECT="at_1"
-DIR=$HOME/"$PROJECT"
+DIR=$(pwd)
 ${PRODUCTION:=true}
 VER=3.12
 
@@ -12,16 +12,19 @@ if ! $PRODUCTION; then
 	brew upgrade
 	brew install python@$VER
 	brew install ffmpeg-full
+	cd
 	python3 -m venv venv
 	. venv/bin/activate
+	cd $DIR
 fi
 python$VER -m pip install --upgrade pip
 pip$VER install -r requirements.txt
 pip$VER install --force-reinstall "faster-whisper @ https://github.com/SYSTRAN/faster-whisper/archive/refs/heads/master.tar.gz"
 if $PRODUCTION; then
+	cd
 	rm -rf $DIR
 	git clone https://github.com/Danielzapirtan/at_1.git
-	cd $DIR
+	cd $PROJECT
 fi
 python$VER app.py $HOME/default.m4a
 cat transcription.txt
